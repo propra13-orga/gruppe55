6efa471d6139802433c6ba1e68fe0a6e3ba4e06a
mvmeegen <markus.van.meegen@uni-duesseldorf.de>
2013-05-11 18:40:57 +0200

    level aktualisiert

--- src/hhu/propra2013/gruppe55/GoalObject.java
+++ src/hhu/propra2013/gruppe55/GoalObject.java
@@ -2,18 +2,12 @@ package hhu.propra2013.gruppe55;
 
 public class GoalObject extends DungeonObject {
 
-	 private String imgPath	=	"img/Goal.png";  // trap-image
+	 private String imgPath	=	"img/Goal.png";  // Goal-image
 		
 	public GoalObject(int x, int y) {
 		super(x,y);
-		state[0].changeImg(imgPath);			  // Aussehen der Falle
-		state[0].massive=false;					  // Falle wird begehbar
+		state[0].changeImg(imgPath);			  // Aussehen des Ziels
+		state[0].massive=false;					  // Ziel wird begehbar
 	}
 	
-	  public void onCollision(DungeonObject d){	 // Tod bei Kollision
-			// hurt the player, yay!
-	    	if(d instanceof	Player)
-				((Player)d).getHit();
-	}   
-
 }
