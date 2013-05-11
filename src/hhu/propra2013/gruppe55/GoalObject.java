bf60ef7d0c6c1157b839d4121f4d764439755eb5
mvmeegen <markus.van.meegen@uni-duesseldorf.de>
2013-05-11 18:02:17 +0200

    Zielklasse und Bild.Levelauswahl ueber Variable.

--- src/hhu/propra2013/gruppe55/GoalObject.java
+++ src/hhu/propra2013/gruppe55/GoalObject.java
@@ -1,12 +1,10 @@
 package hhu.propra2013.gruppe55;
 
+public class GoalObject extends DungeonObject {
 
-
-public class TrapObject extends DungeonObject {
-
-	 private String imgPath	=	"img/Falle.png";  // trap-image
-	
-	public TrapObject(int x, int y) {
+	 private String imgPath	=	"img/Goal.png";  // trap-image
+		
+	public GoalObject(int x, int y) {
 		super(x,y);
 		state[0].changeImg(imgPath);			  // Aussehen der Falle
 		state[0].massive=false;					  // Falle wird begehbar
@@ -16,5 +14,6 @@ public class TrapObject extends DungeonObject {
 			// hurt the player, yay!
 	    	if(d instanceof	Player)
 				((Player)d).getHit();
-	}   	
+	}   
+
 }
